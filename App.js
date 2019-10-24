e0a67a093105f6028ec6bbeed5c4cf28d062c373
김태영 <kimtaeyeong@gimtaeyeong-ui-MacBookPro.local>
2019-10-24 23:16:07 +0900

    Adding Comments

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
-import {Ionicons} from "@expo/vector-icons";
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
 import { AppLoading } from "expo";
-import * as Font from 'expo-font'
-import {Asset} from "expo-asset"
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 import { Text, View, AsyncStorage } from "react-native";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { persistCache } from "apollo-cache-persist";
@@ -13,41 +13,43 @@ import apolloClientOptions from "./apollo";
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
-  const preLoad = async () =>{
-    try{
+  const preLoad = async () => {
+    try {
       await Font.loadAsync({
         ...Ionicons.font
-      })
+      });
       await Asset.loadAsync([require("./assets/logo.png")]);
 
-      const cache = new InMemoryCache();
+      const cache = new InMemoryCache(); // 현재는 비어져 있는 상태
+      // 새로운 캐시를 만듬
       await persistCache({
         cache,
-        storage: AsyncStorage
+        storage: AsyncStorage // 폰에 있는 asyncStorage를 봄(웹사이트의 localStorage랑 비슷)
       });
+      // persist cache는 memory cache에 이는 cache를 가져옴
+
       const client = new ApolloClient({
         cache,
         ...apolloClientOptions
       });
       setLoaded(true);
       setClient(client);
-    }catch(e){
-      console.log(e)
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  useEffect(()=>{
-    preLoad();
-  }, [])
-  return (
-    loaded && client?(
-      <ApolloProvider client={client}>
+  useEffect(() => {
+    preLoad(); //비동기
+  }, []);
+  return loaded && client ? (
+    <ApolloProvider client={client}>
       <View>
-      <Text>Hello</Text>
-    </View>
-      </ApolloProvider>
-
-    ):<AppLoading/>
-
-  )
+        <Text>Hello</Text>
+      </View>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
+    // render를 하면 app의 splash screen을 보여주는 컴포넌트
+  );
 }
