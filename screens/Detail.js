07415c24754e4afe7e9e4488d4557c742fa03576
김태영 <kimtaeyeong@gimtaeyeong-ui-MacBookPro.local>
2019-11-21 16:58:39 +0900

    #16.4 Photo Detail

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,11 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+import { POST_FRAGMENT } from "../fragments";
+import Loader from "../components/Loader";
+import Post from "../components/Post";
+import { ScrollView } from "react-native-gesture-handler";
 
-const View = styled.View``;
-const Text = styled.Text``;
+const POST_DETAIL = gql`
+  query seeFullPost($id: String!) {
+    seeFullPost(id: $id) {
+      ...PostParts
+    }
+  }
+  ${POST_FRAGMENT}
+`;
 
-export default ({ navigation }) => (
-  <View>
-    <Text>I should fetch for : {navigation.getParam("id")}</Text>
-  </View>
-);
+// const View = styled.View``;
+// const Text = styled.Text``;
+
+export default ({ navigation }) => {
+  const { loading, data } = useQuery(POST_DETAIL, {
+    variables: { id: navigation.getParam("id") }
+  });
+
+  return (
+    <ScrollView styled={{ flex: 1 }}>
+      {loading ? (
+        <Loader />
+      ) : (
+        data && data.seeFullPost && <Post {...data.seeFullPost} />
+      )}
+    </ScrollView>
+  );
+};
